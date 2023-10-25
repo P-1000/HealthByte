775af307f10897c2c9ddaf118aa218e9d628003b
pavanpatchikarla <76902617+P-1000@users.noreply.github.com>
2023-10-25 17:50:37 +0530

    navbar

--- Client Frontend/src/App.jsx
+++ Client Frontend/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import {SideBar} from "./components/SideBar/SideBar.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
    <div>
-        <h1>React + Vite + Tailwind CSS</h1>
+    <SideBar/>
    </div>
     </>
   )
