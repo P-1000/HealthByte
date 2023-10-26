3119fd7483be0c806a81089d182f1f1fa449c556
pavanpatchikarla <76902617+P-1000@users.noreply.github.com>
2023-10-26 08:56:32 +0530

    jwt done:

--- back/routes/appointment.js
+++ back/routes/appointment.js
@@ -1,19 +1,28 @@
 import express from "express"
-import user from "../models/user.js"
+import User from "../models/user.js"
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import Appsc from "../models/App.js";
 
 
 const router = express.Router()
 
 
-router.post('/newapp' , (req,res) => {
+router.post('/newapp' , async(req,res) => {
     const {data} = req.body
     try {
         if(!data){
             return res.status(400).json({message:"All fields are required"})
         }
-        const newapp = new user(data)
+        const newapp = await new Appsc({
+            userid : data.patientId,
+            docid : data.doctorId,
+            data : data.selectedDate,
+            time : data.selectedSlot,
+            price : data.selectedSlotPrice,
+        })
+       await newapp.save()
+        res.status(200).json({message:"Appointment booked successfully"})
     } catch (error) {
         console.log(error)
         res.status(500).json({message:"Something went wrong"})
@@ -21,32 +30,32 @@ router.post('/newapp' , (req,res) => {
 }
 )
 
-router.post("/login", async (req,res) => {
-    const {usermain} = req.body
+router.post("/login", async (req, res) => {
+    const { usermain } = req.body;
     try {
-        if(!usermain.uid){
-            res.send(400).json({message:"All fields are required"})
+        if (!usermain.uid) {
+            return res.status(400).json({ message: "All fields are required" }); // Use 'return' to exit the function after sending a response
         }
-        const user = await user.findOne({uid:usermain.uid})
-        const jwt = jwt.sign({id:user._id},'yowaimo' , {expiresIn:"10d"})
-        if(user){
-            return res.status(200).json(jwt)
-        }
-        else{
-            const newuser = await new user(usermain)
-            await newuser.save()
-            res.status(200).json(jwt)
-        }
+        const existingUser = await User.findOne({ userid: usermain.uid });
 
+        if (existingUser) {
+            const token = jwt.sign({ id: existingUser._id }, 'yowaimo', { expiresIn: '10d' });
+            return res.status(200).json({ token }); 
+        } else {
+            const newUser = new User({
+                name: usermain.name,
+                email: usermain.email,
+                mobile: usermain.mobile,
+                userid: usermain.uid,
+            });
+            await newUser.save();
+            const token = jwt.sign({ id: newUser._id }, 'yowaimo', { expiresIn: '10d' });
+            return res.status(200).json({ token }); 
+        }
     } catch (error) {
-        res.status(500).json({message:"Something went wrong"})
+        res.status(500).json({ message: "Something went wrong" });
     }
-
-
-    console.log(usermain.uid)
-
-})
-
+});
 
 
 
