5fc3a48db91f23e63aae824e60f2ed674f7b3fd8
shiva <shivatadigadapa@gmail.com>
2023-10-26 16:14:58 +0530

    done

--- Client Frontend/src/pages/Ecommerce.jsx
+++ Client Frontend/src/pages/Ecommerce.jsx
@@ -20,7 +20,7 @@ const Ecommerce = () => {
     {
       "id": 2,
       "name": "Tylenol",
-      "brand": "McNeil Consumer Healthcare",
+      "brand": "McNeil Consumer ",
       "category": "Pain reliever",
       "description": "Tylenol is a pain reliever and fever reducer. It is available over-the-counter in tablet and liquid form.",
       "price": 9.99,
@@ -40,7 +40,7 @@ const Ecommerce = () => {
     {
       "id": 4,
       "name": "Claritin",
-      "brand": "Pfizer Consumer Healthcare",
+      "brand": "Pfizer Consumer ",
       "category": "Allergy medication",
       "description": "Claritin is an over-the-counter allergy medication that is used to treat symptoms such as sneezing, runny nose, itchy eyes, and hives.",
       "price": 11.99,
@@ -50,17 +50,7 @@ const Ecommerce = () => {
     {
       "id": 4,
       "name": "Claritin",
-      "brand": "Pfizer Consumer Healthcare",
-      "category": "Allergy medication",
-      "description": "Claritin is an over-the-counter allergy medication that is used to treat symptoms such as sneezing, runny nose, itchy eyes, and hives.",
-      "price": 11.99,
-      "quantity": 100,
-      "image": "https://images.unsplash.com/photo-1579182874016-50f3cfba230a?auto=format&fit=crop&q=80&w=2940&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
-    {
-      "id": 4,
-      "name": "Claritin",
-      "brand": "Pfizer Consumer Healthcare",
+      "brand": "Pfizer Consumer ",
       "category": "Allergy medication",
       "description": "Claritin is an over-the-counter allergy medication that is used to treat symptoms such as sneezing, runny nose, itchy eyes, and hives.",
       "price": 11.99,
@@ -74,15 +64,15 @@ const Ecommerce = () => {
       <div className="">
         <h1 className="text-3xl px-6 mt-8 mx-6 font-bold ">Mediceins</h1>
         <div>
-          <div>
-            <h1 className=" px-6  mx-6 font-semibold ">Top deals of pain relief and cough & cold</h1>
+          <div className="mt-14">
+            <h1 className=" px-6   text-2xl mx-6 font-semibold ">Top deals of pain relief and cough & cold</h1>
           </div>
         </div>
       </div>
-      <div className="flex ">
-      <div className="flex p-6 flex-wrap h-[26rem] gap-1">
+      <div className="flex  ">
+      <div className="flex p-6 gap-6 flex-wrap justify-between h-[26rem] ">
       {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
+        <ProductCard key={product.id} product={product}  />
       ))}
     </div>
       </div>
--- Client Frontend/src/utils/ProductCard.jsx
+++ Client Frontend/src/utils/ProductCard.jsx
@@ -1,31 +1,36 @@
 import React from "react";
 
 const ProductCard = ({ product }) => {
-  const { image, name, price, brand , category } = product;
+  const { image, name, price, brand, category } = product;
 
   return (
-    <div className="relative m-1  p-2 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
-      <a href="#" className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl">
+    <div className="relative m-1  p-2 flex w-60  h-[22rem] max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
+      <a
+        href="#"
+        className="relative  mx-3 mt-3 flex h-60 overflow-hidden   rounded-xl"
+      >
         <img className="object-cover" src={image} alt="product image" />
-        <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">
+        <span className="absolute top-0 left-0  w-fit m-2 ml-5 rounded-xl bg-black/60  backdrop-blur-2xl px-2 text-center text-sm font-medium text-white">
           {brand}
         </span>
       </a>
       <div className="mt-5 mx-3 px-3 pb-1 ">
         <a href="#">
           <h5 className="tet-xl tracking-tight text-slate-900">{name}</h5>
-          <span className="text-md font-light text-slate-900/80">{category}</span>
+          <span className="text-md font-light text-slate-900/80">
+            {category}
+          </span>
         </a>
-        <div className="mt-2 mb-5 flex items-center justify-between">
-          <p>
-            <span className="text-3xl font-bold text-slate-900">{price}</span>
-
-          </p>
+        <div className="mt-2 mb-5 flex items-center justify-center">
           <a
             href="#"
-            className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+            className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-xs gap-1 font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
           >
-            <svg
+             <p>
+            <span className="text-xl font-bold text-white/90">{price}</span>
+          </p>
+            Add to cart
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               className="mr-2 h-6 w-6"
               fill="none"
@@ -38,8 +43,7 @@ const ProductCard = ({ product }) => {
                 strokeLinejoin="round"
                 d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
               />
-            </svg>
-            Add to cart
+            </svg> */}
           </a>
         </div>
       </div>
